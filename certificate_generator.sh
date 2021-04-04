36d677c910a41b08ab7166af8bdca053d79e65ae
MorphTail <chitniskshitij@gmail.com>
2021-04-05 01:21:12 +0530

    better output

--- certificate_generator.sh
+++ certificate_generator.sh
@@ -9,6 +9,7 @@ if [ $(which inkscape) = '/usr/bin/inkscape' ]; then
     cat $input_file > tmp/'tmp_'$input_file.txt
     echo -n "Enter Placeholder text: "
     read placeholder_text
+    echo \\n
 
     while IFS='' read -r name || [[ -n "$name" ]]; do
         echo "Generating certificate for $name ..."
@@ -21,10 +22,11 @@ if [ $(which inkscape) = '/usr/bin/inkscape' ]; then
         cp $main_svg_text $named_svg_text
         sed -i "s/$formated_placeholder/$name_for_sed/g" $named_svg_text
         mv $named_svg_text $named_svg
-        inkscape --export-area-page --export-filename=certificates/$formated_name.png $named_svg
+        inkscape --export-area-page --export-filename=certificates/$formated_name.png $named_svg >/dev/null 2>&1
     done < names.txt
 
     rm -rf tmp
+    echo done
 
 else
     echo "Inkscape not installed"
